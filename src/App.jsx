1fd90e341c4552d30d720b84c045737dc1a77c7c
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2025-02-12 08:58:26 -0500

    add questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+const questions = [
+  {
+    questionText: 'What is the capital of France?',
+    answers:[
+      {answerText:'New york',isCorrect:false},
+      {answerText:'London',isCorrect:false},
+      {answerText:'Paris',isCorrect:false},
+      {answerText:'Dublin',isCorrect:false},
+   
+    ]
+  },
+  {
+    questionText: 'What is the capital of Egypt?',
+    answers:[
+      {answerText:'Cairo',isCorrect:false},
+      {answerText:'Riydh',isCorrect:false},
+      {answerText:'Moscow',isCorrect:false},
+      {answerText:'Dubai',isCorrect:false},
+   
+    ]
+  },
 
+]
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <div>
+          <div className=''>Quiz App</div>
+          <div>
+            <div>{questions[0]}</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
